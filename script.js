c18f685acb92d39b184f245634e86ffa05719be1
1et-podwale11 <1et.podwale@gmail.com>
2024-09-10 21:54:48 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
     
     const applyTheme = (e) => {
+        console.log('Zmiana trybu:', e.matches ? 'dark' : 'light'); // Debugowanie
         document.body.classList.toggle('dark-mode', e.matches);
     };
 
