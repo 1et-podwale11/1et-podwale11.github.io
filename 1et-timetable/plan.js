18d115d18f6053b7393bc772be936305b982dc53
1et-podwale11 <1et.podwale@gmail.com>
2024-09-13 22:22:26 +0200

    colors

--- 1et-timetable/plan.js
+++ 1et-timetable/plan.js
@@ -1,7 +1,9 @@
-function wyswietl(sel){
-  var i=sel.selectedIndex
-  if(i!=0) parent.plan.location.href='plany/'+sel.name.charAt(0)+sel.options[i].value+'.html'
+function wyswietl(sel) {
+  var i = sel.selectedIndex;
+  if (i != 0)
+    parent.plan.location.href =
+      "plany/" + sel.name.charAt(0) + sel.options[i].value + ".html";
 }
-function drukuj(){
-    window.print()
+function drukuj() {
+  window.print();
 }
